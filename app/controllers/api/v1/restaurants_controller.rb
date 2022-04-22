a743ff73cc21547ea5125753a038984d0247e0ea
relaxyg <relaxyg4@gmail.com>
2022-04-22 20:42:05 +0600

    add update to api/v1

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,10 +1,31 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :find_restaurant, only: [:show, :update]
   def index
     @restaurants = policy_scope(Restaurant)
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
     authorize @restaurant
+  end
+
+  def update
+    if @restaurant = Restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  private
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+  def restaurant_params
+params.require(@restaurant).permit(:name, :address)
+  end
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
